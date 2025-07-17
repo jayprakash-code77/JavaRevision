b845bfc26633fdd1349b68e0b582b1f2fa362f8f
Jaypraksh Maurya <jayprakashmcode7@gmail.com>
2025-07-17 22:40:07 +0530

    Lecture1 two questions solved

---
+++ JavaCoding/src/Basic/Lecture1.java
@@ -0,0 +1,28 @@
+package Basic;
+
+public class Lecture1 {
+    public static void main(String []args) {
+
+//        add two numbers
+        /*
+        int n1 = 23;
+        int n2 = 34;
+        System.out.println("Sum of two numbers is :"+n1+n2);
+        System.out.println("Sum of two numbers is :"+ (n1+n2));
+        */
+
+
+//        Average of three numbers
+        /*
+        int n1 = 2,n2=3,n3 = 6;
+        int avg = (n1+n2+n3)/3 ;
+        System.out.println(avg);
+
+         */
+
+
+
+
+
+    }
+}
