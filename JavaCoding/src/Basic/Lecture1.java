9d11e5fe42e17eb9b969928d90e5a220b72304c2
Jaypraksh Maurya <jayprakashmcode7@gmail.com>
2025-07-18 23:27:37 +0530

    Lecture 1: Completed!. I all the code of lecture one is done

--- JavaCoding/src/Basic/Lecture1.java
+++ JavaCoding/src/Basic/Lecture1.java
@@ -1,5 +1,5 @@
 package Basic;
-
+import java.util.Scanner;
 public class Lecture1 {
     public static void main(String []args) {
 
@@ -17,10 +17,80 @@ public class Lecture1 {
         int n1 = 2,n2=3,n3 = 6;
         int avg = (n1+n2+n3)/3 ;
         System.out.println(avg);
+         */
+
+
+
+//        check number
+        /*
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        if(num < 0) {
+            System.out.print(-1);
+        }else if (num > 0){
+            System.out.print(1);
+        }else{
+            System.out.print(0);
+        }
+
+         */
+
+//largest of three
+        /*
+        int a = 3;
+        int b = 8;
+        int c = 12;
+
+        if(a > b && a > c) {
+            System.out.print("a is the greater");
+        }else if (b > a && b > c) {
+            System.out.print("b is the greater");
+        }else{
+            System.out.print("c is the greater");
+        }
 
          */
 
 
+
+//        valid triangle
+        /*
+        int s1 = 2;
+        int s2 = 17;
+        int s3 = 3;
+        if(s1+s2 > s3 && s2+s3 > s1 && s1+s3 > s2) {
+            System.out.print("Valid Triangle");
+        }else{
+            System.out.print("Not a valid triangle");
+        }
+         */
+
+
+//      print 1 to N
+        /*
+        int n = 3;
+        for(int i=1; i<=n; i++) {
+            System.out.println(i);
+        }
+
+         */
+
+
+
+//        factorial
+
+        int n = 5;
+        int facto = 1;
+        for(int i=1; i<=n; i++){
+            facto *= i;
+        }
+        System.out.println("Factorial of "+n+" is "+facto);
+
+
+
+
+
+//        check if the number is prime
 
 
 
