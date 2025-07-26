8b551069f6c54c8de5040f9dbcd551c7254e2745
Jaypraksh Maurya <jayprakashmcode7@gmail.com>
2025-07-26 23:08:41 +0530

    Day 2 of the revision

--- JavaCoding/src/Basic/Lecture1.java
+++ JavaCoding/src/Basic/Lecture1.java
@@ -1,4 +1,5 @@
 package Basic;
+import java.lang.Math;
 import java.util.Scanner;
 public class Lecture1 {
     public static void main(String []args) {
@@ -79,6 +80,7 @@ public class Lecture1 {
 
 //        factorial
 
+        /*
         int n = 5;
         int facto = 1;
         for(int i=1; i<=n; i++){
@@ -86,11 +88,83 @@ public class Lecture1 {
         }
         System.out.println("Factorial of "+n+" is "+facto);
 
+         */
+
 
 
 
 
 //        check if the number is prime
+
+        int n = 15;
+        boolean isPri = true;
+        if(n <= 1) {
+            isPri = false;
+        }else{
+            for(int i=2; i<=Math.sqrt(n); i++) {
+                if(i % n == 0) {
+                    isPri = false;
+                    break;
+                }
+            }
+
+        }
+        if(isPri) {
+            System.out.print(n+" is a prime number.");
+        } else {
+            System.out.print(n+" is not a prime number.");
+        }
+
+
+
+
+        /*
+        Problem statement
+You are given a single positive integer, N. You need to print all even integers that occur between 1 and N (both inclusive).
+
+Draw a flowchart for this process
+
+Note : You don't need to submit the problem. Just attempt in your notebook and ask doubts if you need help.
+
+         */
+
+        /*
+        int n=20;
+        for(int i =1; i<=n; i++) {
+            if(i%2 == 0) {
+                System.out.print(i+" ");
+            }
+        }
+         */
+
+
+
+
+
+//        largest of N numbers
+        /*
+        int[] num = {1, 23, 45, 6, 23, 11, 2};
+        int largest = num[0];
+        for(int i=1; i<num.length; i++) {
+            if(num[i] > largest) {
+                largest = num[i];
+            }
+        }
+        System.out.print("Largest number is "+largest);
+
+         */
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
