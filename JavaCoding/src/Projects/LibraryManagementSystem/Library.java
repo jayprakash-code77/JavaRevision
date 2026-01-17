0e66136084eb6966f8336c7977533c03324d6bcb
Jaypraksh Maurya <jayprakashmcode7@gmail.com>
2026-01-17 23:58:48 +0530

    Project: Started building the Library Management System

---
+++ JavaCoding/src/Projects/LibraryManagementSystem/Library.java
@@ -0,0 +1,8 @@
+package Projects.LibraryManagementSystem;
+
+public class Library {
+    public static void main(String[] args) {
+        System.out.println("I am Library. Need books? Please follow " +
+                "the instruction to: Issue a book, return a book, read a book");
+    }
+}
