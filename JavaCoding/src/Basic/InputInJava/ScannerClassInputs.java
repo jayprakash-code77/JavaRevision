458d887944baa40ea56a1ad7bd8a704f418115fd
Jayprakash Maurya <jayprakashmcode7@gmail.com>
2026-05-12 12:09:22 +0530

    Input iin Java: Finished Scanner Class Input In Java

---
+++ JavaCoding/src/Basic/InputInJava/ScannerClassInputs.java
@@ -0,0 +1,11 @@
+package Basic.InputInJava;
+
+import java.util.Scanner;
+
+public class ScannerClassInputs {
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        System.out.println(age);
+    }
+}
