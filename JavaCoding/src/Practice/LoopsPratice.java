957e7b47a9f899a438e872533e8efe9c5632d223
Jayprakash Maurya <jayprakashmcode7@gmail.com>
2026-05-14 02:45:29 +0530

    Finished Loops and Conditionals in java

---
+++ JavaCoding/src/Practice/LoopsPratice.java
@@ -0,0 +1,117 @@
+package Practice;
+
+public class LoopsPratice {
+    public static void main(String[] args) {
+
+        //counting from 1 to n
+        /*
+        int n = 10;
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i);
+        }
+
+         */
+
+
+        // n to 1 counting
+        /*
+        int n = 8;
+        for (int i = n; i >= 1; i--) {
+            System.out.println(i);
+        }
+
+         */
+
+
+
+
+
+
+        // 10 multiple of n
+        /*
+        int n = 5;
+        for(int i=1; i<=10; i++) {
+            System.out.println(i*n);
+        }
+
+         */
+
+
+
+
+
+
+
+
+
+        //  1 to 100 : even numbers
+        /*
+        for(int i=2; i<=100; i++) {
+            if(i%2 == 0) {
+                System.out.println(i);
+            }
+        }
+
+         */
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // print sum of numbers from 1 to n
+        /*
+        int sum = 0;
+        int n = 5;
+        while(n >= 0) {
+            sum += n;
+            n--;
+        }
+        System.out.println(sum);
+
+         */
+
+
+
+
+
+
+
+        // in the range [50, 100] : print perfectly divisible by 7
+        /*
+        for(int i=50; i<=100; i++) {
+            if(i%7 == 0) {
+                System.out.println(i);
+            }
+        }
+
+         */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
