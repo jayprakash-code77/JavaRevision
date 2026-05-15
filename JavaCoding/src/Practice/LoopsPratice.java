8361cff9d988090e8dc52f2be9685dcbd620b7ec
Jayprakash Maurya <jayprakashmcode7@gmail.com>
2026-05-15 15:00:42 +0530

    Solved some classical question focusing on loops

--- JavaCoding/src/Practice/LoopsPratice.java
+++ JavaCoding/src/Practice/LoopsPratice.java
@@ -101,6 +101,180 @@ public class LoopsPratice {
 
 
 
+        // factorial of a number
+        /*
+        int num = 5;
+        int fact = 1;
+        while( num >= 1) {
+            fact *= num;
+            num--;
+        }
+        System.out.println("Factorial: "+fact);
+
+         */
+
+
+
+
+
+
+
+
+
+
+        // reverse the number
+        /*
+        int num = 1245;
+        int reverse = 0;
+        while(num > 0) {
+            int lastDigit = num % 10;
+            reverse = (reverse * 10) + lastDigit;
+            num = num / 10;
+        }
+        System.out.println(reverse);
+
+         */
+
+
+
+
+
+
+
+        // check palindrome number: 1st approach
+        /*
+        int num = 12421;
+        int originalNum = num;
+        int reverse = 0;
+        while(num > 0) {
+            int lastDigit = num % 10;
+            reverse = (reverse * 10) + lastDigit;
+            num = num / 10;
+        }
+        if(reverse == originalNum) {
+            System.out.println("Palindrome");
+        }else{
+            System.out.println("Not a Palindrome");
+        }
+
+         */
+
+
+
+
+
+
+
+
+
+        // count digits of a number
+        /*
+        int num = 435;
+        int noOfDigits = 0;
+        while (num > 0) {
+            noOfDigits++;
+            num = num / 10;
+        }
+        System.out.println("No. of digits: "+noOfDigits);
+
+         */
+
+
+
+
+
+
+
+
+
+        // sum of digits
+        /*
+        int num = 2234;
+        int sumOfDigits = 0;
+        while(num > 0) {
+            sumOfDigits = sumOfDigits + num%10;
+            num = num / 10;
+        }
+        System.out.println("Sum of digits: "+sumOfDigits);
+
+         */
+
+
+
+
+
+
+
+
+        // Fibonacci Series
+        /*
+        int limit = 10;
+        int f1 = 0;
+        int f2 = 1;
+        System.out.println(f1+"\n"+f2);
+        while(limit -2 > 0) {
+            int next = f1+f2;
+            System.out.println(next);
+            f1 = f2;
+            f2 = next;
+
+            limit--;
+        }
+
+         */
+
+
+
+
+
+
+
+
+
+
+
+        // Prime Number
+        /*
+        int num = 23;
+        boolean isPrime = true;
+        for(int i=2; i<(num/2)+1; i++) {
+            if(num%i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if(isPrime) {
+            System.out.println("Prime");
+        }else{
+            System.out.println("Not Prime");
+        }
+
+         */
+
+        // Prime number using the Mathematical Way
+        int num = 23;
+        boolean isPrime = true;
+        for(int i=2; i*i <= num; i++) {
+            if(num%i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) {
+            System.out.println("Prime");
+        }else{
+            System.out.println("Not Prime");
+        }
+
+
+
+
+
+
+
+
+
+
 
 
 
